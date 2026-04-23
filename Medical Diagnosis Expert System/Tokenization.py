7ed1f1d35d3a3120be302b43b2c1380e1f0690ce
asmaa <20231700208@cis.asu.edu.eg>
2026-04-23 23:41:33 +0200

    minimal modularity + function clarifying q + main ready for testing

--- Medical Diagnosis Expert System/Tokenization.py
+++ Medical Diagnosis Expert System/Tokenization.py
@@ -1,10 +1,12 @@
-import data_prep as data
-from functools import reduce
-from nltk import word_tokenize
-from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from experta import *
+import data_prep as data
+from nltk.corpus import stopwords
+import nltk
+from collections import Counter
+
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 stop_words = set(stopwords.words('english'))
@@ -12,93 +14,44 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 patterns, all_symptoms = data.load_data()
 
-def load_stemmed_symptom_dict():
-    
-    stemmed_symptom_dict = {}
-    for symptom in all_symptoms:
-
-        words = symptom.replace("_", " ")
-        words = tokenizer.tokenize(words)
-        words = [w for w in words if w not in stop_words]
-
-        stemmed_words = frozenset(ps.stem(w) for w in words)
-        stemmed_symptom_dict[stemmed_words] = symptom
-    
-    return stemmed_symptom_dict
-
-stemmed_symptom_dict = load_stemmed_symptom_dict()
-
 def process_user_input(user_input):
-    
     tokens = tokenizer.tokenize(user_input)
     filtered_tokens = [word for word in tokens if word not in stop_words]
-    
+
     stemmed_tokens = []
     for w in filtered_tokens:
-        w = w.replace("_"," ")
+        w = w.replace("_", " ")
         w = ps.stem(w)
         stemmed_tokens.append(w)
-    
+
     return stemmed_tokens
 
-def match_symptoms(stemmed_tokens):
+
+def match_symptoms(stemmed_tokens, stemmed_dict):
     matched_symptoms = []
-    for stemmed_key, original_symptom in stemmed_symptom_dict.items():
+    for stemmed_key, original_symptom in stemmed_dict.items():
         if stemmed_key.issubset(stemmed_tokens):
             matched_symptoms.append(original_symptom)
     return matched_symptoms
+
 
 def no_matched_symptoms(matched_symptoms):
     if len(matched_symptoms) == 0:
         print("\nSorry, I could not recognize any symptoms from what you described.")
         print("Please try to describe your symptoms more clearly.")
         print("Example: 'I have itching, skin rash and fever'")
-    return True 
+    return True
 
-def extract_symptoms(user_input):
+
+def extract_symptoms(user_input, stemmed_dict):
     stemmed_tokens = process_user_input(user_input)
-    matched_symptoms = match_symptoms(stemmed_tokens)
+    matched_symptoms = match_symptoms(stemmed_tokens, stemmed_dict)
     no_matched_symptoms(matched_symptoms)
     print(matched_symptoms)
     return matched_symptoms
-    
-text = input("Describe your symptoms: ")
-matched_symptoms = extract_symptoms(text)
 
 
 
 
-#task 2
 
-#prepare the data
-from experta import *
-
-def matched_diseases():
-    class diseases_symptom(Fact):
-        pass
-    class diseases_matched(Fact):
-        pass
-    class MedicalExpertSystem(KnowledgeEngine):
-        def __init__(self,matched_symptoms,knowledge_base):
-            super().__init__()
-            self.matched_symptoms = matched_symptoms
-            self.knowledge_base = knowledge_base
-        @DefFacts()
-        def _initial_action(self):
-            # yield Fact(action="find_disease")
-            for symptom in self.matched_symptoms:
-                yield diseases_symptom(name=symptom)
-        @Rule()
-        def diagonise(self):
-            for obj in self.knowledge_base:
-                #i want to get the intersection with eah disease and if it is not empty, add the matched as facts and missed symptomps also 
-                matched = set(obj["symptoms"]).intersection(set(self.matched_symptoms))
-                if matched:
-                    missed = set(obj["symptoms"]).difference(set(self.matched_symptoms))
-                    self.declare(diseases_matched(id=obj["id"], 
-                                                name=obj["name"],
-                                                matched=list(matched),
-                                                missed=list(missed),
-                                                counter=len(matched),
-                                                ))
 
--- Medical Diagnosis Expert System/main.py
+++ Medical Diagnosis Expert System/main.py
@@ -1,3 +1,69 @@
+import data_prep as data
 import Tokenization as tk
+from ExpertSystem import MedicalExpertSystem, diseases_matched
+from collections import Counter
+from functools import reduce
+from nltk import word_tokenize
+
+patterns, all_symptoms = data.load_data()
+already_asked = []
+
+def load_stemmed_symptom_dict():
+    stemmed_symptom_dict = {}
+    for symptom in all_symptoms:
+        words = symptom.replace("_", " ")
+        words = tk.tokenizer.tokenize(words)
+        words = [w for w in words if w not in tk.stop_words]
+
+        stemmed_words = frozenset(tk.ps.stem(w) for w in words)
+        stemmed_symptom_dict[stemmed_words] = symptom
+
+    return stemmed_symptom_dict
+
+
+stemmed_symptom_dict = load_stemmed_symptom_dict()
 text = input("Describe your symptoms: ")
-tk.extract_symptoms(text)
+matched_symptoms = tk.extract_symptoms(text,stemmed_symptom_dict)
+
+
+#res is the diseases with 0.50 or less cf
+def ask_clarifying_questions(res, current_matched_symptoms):
+    potential_symp=[]
+    for disease in res:
+        potential_symp.extend(disease['missed'])
+    counts = Counter(potential_symp)
+    # shared symptoms between diseases
+    sorted_symp = list(counts)
+
+    for symp in sorted_symp:
+        if symp in already_asked:
+            continue
+        else:
+            answer = input(f"Are you experiencing {symp}? (y/n): ").strip().lower()
+            already_asked.append(symp)
+            if answer == "y":
+                current_matched_symptoms.append(symp)
+                print(current_matched_symptoms)
+                return True
+    print(current_matched_symptoms)
+    return False
+
+
+if matched_symptoms:
+    while True:
+        engine = MedicalExpertSystem(matched_symptoms, patterns)
+        engine.reset()
+        engine.run()
+        #sort according to the CF (descending)
+        results = [dict(fact) for fact in engine.facts.values() if isinstance(fact, diseases_matched)]
+        results.sort(key=lambda x: x['cf'], reverse=True)
+
+        if results and results[0]['cf'] < 0.8:
+            if not ask_clarifying_questions(results, matched_symptoms):
+                break
+        else:
+            break
+
+
+
+
